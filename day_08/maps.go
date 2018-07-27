422a96800ac1a42e9b155206666ac9a065c83d00
Unknown <vananger93@gmail.com>
2018-07-26 22:42:43 -0600

    day_08 failed the tests now fixed
    made an assumption that they would submit as many tests as entries

--- day_08/maps.go
+++ day_08/maps.go
@@ -1,52 +1,54 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
 )
 
 
 func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
 
-	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
-	checkError(err)
-	n := int32(nTemp)
-	m := make(map[string]int64)
+    nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+    m := make(map[string]int64)
 
-	for i := 0; i < int(n); i++ {
-		arrTemp := strings.Split(readLine(reader), " ")
-		tempItem, err := strconv.ParseInt(arrTemp[1], 10, 64)
-		checkError(err)
-		m[arrTemp[0]] = tempItem
-	}
+    for i := 0; i < int(n); i++ {
+        arrTemp := strings.Split(readLine(reader), " ")
+        tempItem, err := strconv.ParseInt(arrTemp[1], 10, 64)
+        checkError(err)
+        m[arrTemp[0]] = tempItem
+    }
 
-	for i := 0; i < int(n); i++ {
-		arrTemp := readLine(reader)
-		if _, ok := m[arrTemp]; ok {
-			fmt.Printf("%s=%d\n", arrTemp, m[arrTemp])
-		} else {
-			fmt.Println("Not found")
-		}
-	}
+    for {
+        arrTemp := readLine(reader)
+        if arrTemp == "" {
+            break
+        }
+        if _, ok := m[arrTemp]; ok {
+            fmt.Printf("%s=%d\n", arrTemp, m[arrTemp])
+        } else {
+            fmt.Println("Not found")
+        }
+    }
 
 }
 
 func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
-	return strings.TrimRight(string(str), "\r\n")
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+    return strings.TrimRight(string(str), "\r\n")
 }
 
 func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }
-
